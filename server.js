07ae77c54fbff1d700c2b0d42437d75aad7d6cc0
dyaskur <dyas@yaskur.com>
2019-05-05 01:09:06 +0700

    change port and add IP

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
+import ip from 'ip';
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './schema';
 import cors from 'cors';
 
-const GRAPHQL_PORT = 4000;
+const GRAPHQL_PORT = 80;
 
 const graphQLServer = express();
-graphQLServer.use('*', cors({ origin: 'http://192.168.1.3:8081' }));
+graphQLServer.use('*', cors({ origin: 'http://192.168.1.1:4000' }));
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+const ipz  = ip.address();
 
-
-graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
+graphQLServer.listen(GRAPHQL_PORT,ipz, () => console.log(
+    `GraphiQL is now running on http://${ipz}:${GRAPHQL_PORT}/graphiql`
 ));
